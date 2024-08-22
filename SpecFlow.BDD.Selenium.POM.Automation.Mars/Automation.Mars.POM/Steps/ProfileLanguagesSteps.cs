0845f6a8930036c2ea77d65d4b024dffbb774d97
FocusOnTesting <jay.poung@gmail.com>
2024-08-23 09:01:55 +1200

    Implement Languages feature Add single and multiple languages scenarios

--- SpecFlow.BDD.Selenium.POM.Automation.Mars/Automation.Mars.POM/Steps/ProfileLanguagesSteps.cs
+++ SpecFlow.BDD.Selenium.POM.Automation.Mars/Automation.Mars.POM/Steps/ProfileLanguagesSteps.cs
@@ -1,7 +1,12 @@
-﻿using Automation.Mars.POM.WebAbstraction;
+﻿using Automation.Mars.Core.Abstraction;
+using Automation.Mars.POM.WebAbstraction;
+using AventStack.ExtentReports.Model;
+using Serilog;
+using RazorEngine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
@@ -17,8 +22,8 @@ namespace Automation.Mars.POM.Steps
             _iprofilePageLanguages = iprofilePageLanguages;
         }
 
-        [When(@"Clean up stale languages")]
-        public void WhenCleanUpStaleLanguages()
+        [When(@"Clean up languages")]
+        public void WhenCleanUpLanguages()
         {
             _iprofilePageLanguages.CleanUpLanguages();
         }
@@ -26,37 +31,57 @@ namespace Automation.Mars.POM.Steps
         [Then(@"No language is in the table")]
         public void ThenNoLanguageIsInTheTable()
         {
-            Assert.Zero(_iprofilePageLanguages.NumberOfLanguages());
+            Assert.Zero(_iprofilePageLanguages.CountOfLanguages());
         }
 
         [When(@"Click Add New button")]
         public void WhenClickAddNewButton()
         {
+            _iprofilePageLanguages.ClickAddNewButton();
         }
 
         [When(@"Input a language '([^']*)'")]
-        public void WhenInputALanguage(string english)
+        public void WhenInputALanguage(string language)
         {
+            _iprofilePageLanguages.InputLanguageName(language);
         }
 
         [When(@"Choose a language level '([^']*)'")]
-        public void WhenChooseALanguageLevel(string fluent)
+        public void WhenChooseALanguageLevel(string level)
         {
+            _iprofilePageLanguages.SelectLanguageLevel(level);
         }
 
         [When(@"Click Add button")]
         public void WhenClickAddButton()
         {
+            _iprofilePageLanguages.ClickAddButton();
         }
+
+        [When(@"Add Languages")]
+        public void WhenAddLanguages(Table table)
+        {
+            IDictionary<string, string> dictionary = new Dictionary<string, string>();
+            foreach (TableRow row in table.Rows)
+            {
+                _iprofilePageLanguages.ClickAddNewButton();
+                _iprofilePageLanguages.InputLanguageName(row[0]);
+                _iprofilePageLanguages.SelectLanguageLevel(row[1]);
+                _iprofilePageLanguages.ClickAddButton();
+            }
+        }
+
 
         [Then(@"A successful message pop is shown on the right top")]
         public void ThenASuccessfulMessagePopIsShownOnTheRightTop()
         {
+
         }
 
-        [Then(@"Clean up test language datas")]
-        public void ThenCleanUpTestLanguageDatas()
+        [Then(@"Clean up test languages")]
+        public void ThenCleanUpTestLanguages(Table table)
         {
+
         }
 
     }
